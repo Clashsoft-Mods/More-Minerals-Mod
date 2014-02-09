092eb7cd14026faf8deff7eea06b9d23c7c5f991
Clashsoft <clash2@hotmail.de>
2014-02-09 17:57:29 +0100

    Update to 1.7.2
    Total rewrite

--- client/MMMClientProxy.java
+++ client/MMMClientProxy.java
@@ -1,22 +1,7 @@
 package clashsoft.mods.moreminerals.client;
 
-import clashsoft.mods.moreminerals.client.renderer.block.MMMOreRenderer;
 import clashsoft.mods.moreminerals.common.MMMCommonProxy;
-import cpw.mods.fml.client.registry.RenderingRegistry;
 
 public class MMMClientProxy extends MMMCommonProxy
 {
-	public static int	oreRenderer;
-	
-	@Override
-	public void registerBlockRenderers()
-	{
-		oreRenderer = RenderingRegistry.getNextAvailableRenderId();
-		RenderingRegistry.registerBlockHandler(oreRenderer, new MMMOreRenderer());
-	}
-	
-	@Override
-	public void registerItemRenderers()
-	{
-	}
 }
--- common/MMMCommonProxy.java
+++ common/MMMCommonProxy.java
@@ -1,44 +1,21 @@
 package clashsoft.mods.moreminerals.common;
 
-import clashsoft.mods.moreminerals.MoreMineralsMod;
-import clashsoft.mods.moreminerals.client.gui.GuiOreCrusher;
-import clashsoft.mods.moreminerals.inventory.ContainerOreCrusher;
-import clashsoft.mods.moreminerals.tileentity.TileEntityOreCrusher;
+import cpw.mods.fml.common.network.IGuiHandler;
 
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
-
-import cpw.mods.fml.common.network.IGuiHandler;
 
 public class MMMCommonProxy implements IGuiHandler
 {
 	@Override
 	public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z)
 	{
-		TileEntity te = world.getBlockTileEntity(x, y, z);
-		if (te instanceof TileEntityOreCrusher || ID == MoreMineralsMod.OreCrusher_TEID)
-		{
-			return new ContainerOreCrusher(player.inventory, (TileEntityOreCrusher) te);
-		}
 		return null;
 	}
 	
 	@Override
 	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z)
 	{
-		if (ID == MoreMineralsMod.OreCrusher_TEID)
-		{
-			return new GuiOreCrusher(player.inventory, (TileEntityOreCrusher) world.getBlockTileEntity(x, y, z));
-		}
 		return null;
-	}
-	
-	public void registerItemRenderers()
-	{
-	}
-	
-	public void registerBlockRenderers()
-	{
 	}
 }
